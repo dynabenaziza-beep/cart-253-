5b0c5f728a740b6abe75d520b01105475cd654a6
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-09-20 22:49:08 -0400

    made the rectangle(close up of my eye ) interactive to be able to drag it
    -added the drag and drop
    -made the pupil and iris move with the rectangle

--- art-jam/js/script.js
+++ art-jam/js/script.js
@@ -1,17 +1,39 @@
+// variable to make the rectangle interactive 
+// x cordinate of top left
+let rectX= 150;
+// y cordinate of top left 
+let rectY= 100
+//width 
+let rectW= 250
+//heigth of the rectangle
+let rectH= 190
+//
+let dragging= false;
+
 
 function setup() {
   createCanvas(800,800);
 
 
+
 }
 
 
-function draw(){
+function draw() {
 background(255);
+
+
+//when dragging it will update the position of the rectangle 
+if (dragging) {
+    rectX = mouseX;
+    rectY = mouseY;
+}
 // collor the rectangle beige to match my skin color 
 fill(245,230,200);
 //draw the rectangle to be at the positon i want it to be 
-rect(150,100,250,190);
+rect(rectX, rectY,rectW,rectH);
+
+
 
 
 
@@ -21,9 +43,9 @@ fill(255);
 
 //draw a more almond shape for the eye 
 beginShape();
-vertex(150, 195);
-bezierVertex(200 ,90, 350, 90, 400, 195);//top
-bezierVertex(350,300,200,300,150,195);// bottom
+vertex(rectX, rectY+95);
+bezierVertex(rectX+50 ,rectY-10, rectX+200, rectY-10, rectX+250, rectY+95);//top
+bezierVertex(rectX+200,rectY+200,rectX+50,rectY+200,rectX,rectY+95);// bottom
 endShape(CLOSE);
 
 
@@ -31,20 +53,36 @@ endShape(CLOSE);
 //draw the iris of the eye (green)
 noStroke();  
 fill(0,255,0);
-ellipse(270,200,90,160);
+ellipse(rectX+120,rectY+100,90,160);
 
 //draw the pupil 
 fill(0);
-ellipse(270,200,30,50);
+ellipse(rectX+120,rectY+100,30,50);
 
 //#1 white highlight 
 fill(255);
-ellipse(290,185,20,40)
+ellipse(rectX+140,rectY+85,20,40);
 //#2 white highlight 
 fill(255);
-ellipse(240,155,45,50)
-
-
+ellipse(rectX+90,rectY+55,45,50);
+}
+//
+function mousePressed(){
+//if i touche the rectangle it need to be interactive and moove 
+if (mouseX > rectX && mouseX < rectX + rectW && 
+  mouseY > rectY && mouseY <  rectY + rectH) {
+    dragging=true;
+  } 
+  
+}
+//when release rectangle= stop moving 
+function mouseReleased() {
+dragging= false;
 
 }
+
+
+
+
+  
 
