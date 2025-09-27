6aecddff32304b9cf0790d11df6c0802829090d2
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-09-27 16:20:32 -0400

    create interactive rectangle #2
    the second interactive rectangle will be a middle close up of my left eye

--- art-jam/js/script.js
+++ art-jam/js/script.js
@@ -1,4 +1,4 @@
-// variable to make the rectangle interactive 
+// variable to make the rectangle (interactive piece #1)
 // x cordinate of top left
 let rectX= 150;
 // y cordinate of top left 
@@ -8,7 +8,14 @@ let rectW= 250
 //heigth of the rectangle
 let rectH= 190
 //
-let dragging= false;
+let dragging1= false;
+// variable to make the ( interactive piece #2)
+let rect2X= 400 ;
+let rect2Y= 300;
+let rect2W=300;
+let rect2H= 150;
+let dragging2 = false;
+
 
 
 function setup() {
@@ -24,15 +31,24 @@ background(255);
 
 
 //when dragging it will update the position of the rectangle 
-if (dragging) {
+if (dragging1) {
     rectX = mouseX;
     rectY = mouseY;
+} 
+
+if (dragging2) {
+    rect2X = mouseX;
+    rect2Y = mouseY;
 }
+//rect1 
 // collor the rectangle beige to match my skin color 
 fill(245,230,200);
 //draw the rectangle to be at the positon i want it to be 
 rect(rectX, rectY,rectW,rectH);
 
+//rect2
+fill(245,230,200);
+rect(rect2X,rect2Y,rect2W ,rect2H);
 
 
 
@@ -68,16 +84,22 @@ ellipse(rectX+90,rectY+55,45,50);
 }
 //
 function mousePressed(){
+  //rectangle#1
 //if i touche the rectangle it need to be interactive and moove 
 if (mouseX > rectX && mouseX < rectX + rectW && 
   mouseY > rectY && mouseY <  rectY + rectH) {
-    dragging=true;
+    dragging1=true;
   } 
-  
+  //rectangle #2 
+  if (mouseX > rect2X && mouseX < rect2X + rect2W && 
+  mouseY > rect2Y && mouseY <  rect2Y + rect2H) {
+    dragging2=true;
+  } 
 }
 //when release rectangle= stop moving 
 function mouseReleased() {
-dragging= false;
+dragging1= false;
+dragging2= false;
 
 }
 
