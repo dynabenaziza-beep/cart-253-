2773beb03fd5de00162e275bef01f3e8207a79e5
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-10-12 15:56:58 -0400

    start the event challenge
    keep count of score

--- event-challenge/js/script.js
+++ event-challenge/js/script.js
@@ -1,57 +1,22 @@
-// events  challenge 
+//events challenges 
 
-"use strict";
+let scrore= 0; // show the current score 
+let gameOver = false //keep track of the point we lost 
 
-// Current score
-let score = 0;
-
-// Is the game over?
-let gameOver = false;
-
-/**
- * Create the canvas
- */
-function setup() {
-  createCanvas(400, 400);
+function setup(){
+createCanvas(400,400);
 }
 
-/**
- * Update the score and display the UI
- */
-function draw() {
-  background("#87ceeb");
-  
-  // Only increase the score if the game is not over
-  if (!gameOver) {
-    // Score increases relatively slowly
-    score += 0.05;
-  }
-  displayUI();
+function draw(){
+  background(#)
 }
 
-/**
- * Show the game over message if needed, and the current score
- */
-function displayUI() {
-  if (gameOver) {
-    push();
-    textSize(48);
-    textStyle(BOLD);
-    textAlign(CENTER, CENTER);
-    text("You lose!", width/2, height/3);
-    pop();
-  }
-  displayScore();
+if (!gameOver) {
+  score+= 0.05;
 }
 
-/**
- * Display the score
- */
-function displayScore() {
-  push();
-  textSize(48);
-  textStyle(BOLD);
-  textAlign(CENTER, CENTER);
-  text(floor(score), width/2, height/2);
-  pop();
-}
+push();
+textAlign(CENTRE,CENTER); 
+textSize(48);
+text(floor(score,width/2, height/2));
+pop();
