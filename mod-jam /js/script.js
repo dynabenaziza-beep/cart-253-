519e8a5216d1dfe702d82ce0a4dc916beddec166
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-10-21 16:33:39 -0400

    started mod-jam

---
+++ mod-jam /js/script.js
@@ -0,0 +1,24 @@
+/**
+ * mod-jam
+ * Title of Project
+ * dyna benaziza 
+ * 
+
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
