c939438cf2051454849b743d2e8bf8473aa448c6
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-10-23 14:43:06 -0400

    drawing basic frog body
    i added a drawFrog function () in draw() to make frog show up on screen

--- mod-jam /js/script.js
+++ mod-jam /js/script.js
@@ -7,7 +7,16 @@
  */
 
 "use strict";
+//FROG 
 
+const frog ={
+body:{
+    x: 320,
+    y: 520,
+    size: 150 
+}
+
+};
 /**
  * Creates the canvas and initializes the fly 
 */
@@ -15,12 +24,19 @@ function setup() {
  createCanvas(640,480);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-    background("#87ceeb");
+    background("#87ceeb"); // creating the background ( bleu) for the game 
+    drawFrog();
 
+}
+
+function drawFrog() {
+
+    //Frog body
+    push();
+    fill("#00ff00");
+    noStroke();
+    ellipse(frog.body.x, frog.body.y, frog.body.size);
+    pop();
 
 }
