b5aeaa6559ae86a3859b7948a736de8de2373939
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-10-27 12:04:06 -0400

    add frog tongue in drawFrog

--- mod-jam /js/script.js
+++ mod-jam /js/script.js
@@ -13,11 +13,21 @@ const frog ={
 body:{
     x: 320,
     y: 520,
-    size: 220
+    size: 220,
+
+},
+tongue: {
+  x: 320,
+  y: 520, 
+  size: 20, 
+  length:100 
 }
 
 };
-/**
+
+
+
+/** 
  * Creates the canvas and initializes the fly 
 */
 function setup() {
@@ -28,6 +38,7 @@ function draw() {
     background("#87ceeb"); // creating the background ( bleu) for the game 
     drawFrog();
   moveFrog();
+
 
 }
 
@@ -42,12 +53,19 @@ function drawFrog() {
     
 
     //frog ears
-    
-    push();
+     push();
   fill("#00ff00");
   noStroke();
   ellipse(frog.body.x - 60,frog.body.y- 100,80,75); //left frog ear 
   ellipse(frog.body.x + 70,frog.body.y-100,80,75); //rigth frog ear  
+pop();
+
+//
+push();
+stroke("#ff0000");
+strokeWeight(frog.tongue.size);
+line(
+  frog.body.x, frog.body.y, frog.tongue.x, frog.body.y - frog.tongue.length );
 pop();
 }
 function moveFrog(){
