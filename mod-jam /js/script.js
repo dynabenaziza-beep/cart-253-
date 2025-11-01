354a698eeb36a3c74585e050d43367ac189d5084
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-11-01 15:29:23 -0400

    changed single fly to multiple flies using and array  and changed the movement
    i chnaged the code so now there more than one flies instead of just one . i made a flies array with two flies , one come form the right and the other come from the left .  finally i chnaged all the fly game part from the frog game to flies or for (let fly of flies )

--- mod-jam /js/script.js
+++ mod-jam /js/script.js
@@ -29,12 +29,15 @@ const frog = {
 
 // Our fly
 // Has a position, size, and speed of horizontal movement
-const fly = {
-    x: 0,
-    y: 200, // Will be random
-    size: 10,
-    speed: 3
-};
+//const fly = {
+   // x: 0,
+   // y: 200, // Will be random
+   // size: 10,
+   // speed: 3
+//};
+
+let flies = []; // empty list to store flies 
+
 
 /**
  * Creates the canvas and initializes the fly
@@ -42,51 +45,86 @@ const fly = {
 function setup() {
     createCanvas(640, 480);
 
-    // Give the fly its first random position
-    resetFly();
+    // one fly come from the left 
+    flies.push({
+        x: 0 , // start left edge 
+        y: 200,
+        size: 10, 
+        speedX: 3  // how fast it goes  , move rigth 
+
+});
+
+
+flies.push({
+x: 640,  //start rigth edge 
+y : 150,
+size: 10, 
+speedX: -3  // move left 
+
+}); 
 }
 
+/**
+ * Creates the canvas and initializes the fly
+ */
+
+    
+
 function draw() {
-    background("#87ceeb");
-    moveFly();
-    drawFly();
+   background("#87ceeb");
+   
+    //move the flies 
+    moveFlies();
+    drawFlies();
+
+
+   
     moveFrog();
     moveTongue();
     drawFrog();
-    checkTongueFlyOverlap();
+
+    //check if tongue touch flies 
+    checkTongueFliesOverlap();
 }
 
 /**
  * Moves the fly according to its speed
  * Resets the fly if it gets all the way to the right
  */
-function moveFly() {
-    // Move the fly
-    fly.x += fly.speed;
-    // Handle the fly going off the canvas
-    if (fly.x > width) {
-        resetFly();
-    }
+function moveFlies() {
+   for (let fly of flies ){
+
+  fly.x = fly.x + fly.speedX; //update position frames 
+    
+//if fly goes too much left sie = go back to left 
+if(fly.x > 640){
+  fly.x = 0;
+  fly.y = random(50,300);
 }
+// if the fly goes too much left side = go rigth 
+if(fly.x< 0){
+  fly.x = 640;
+  fly.y= random(50,300);
+}
+   }
+  }
 
 /**
  * Draws the fly as a black circle
  */
-function drawFly() {
+function drawFlies() {
+  for(let fly of flies) { //call all flies 
     push();
     noStroke();
     fill("#000000");
     ellipse(fly.x, fly.y, fly.size);
     pop();
+  }
 }
 
 /**
  * Resets the fly to the left with a random y
  */
-function resetFly() {
-    fly.x = 0;
-    fly.y = random(0, 300);
-}
 
 /**
  * Moves the frog to the mouse position on x
@@ -117,7 +155,7 @@ function moveTongue() {
     else if (frog.tongue.state === "inbound") {
         frog.tongue.y += frog.tongue.speed;
         // The tongue stops if it hits the bottom
-        if (frog.tongue.y >= height) {
+        if (frog.tongue.y >= 480) {
             frog.tongue.state = "idle";
         }
     }
@@ -152,17 +190,19 @@ function drawFrog() {
 /**
  * Handles the tongue overlapping the fly
  */
-function checkTongueFlyOverlap() {
-    // Get distance from tongue to fly
-    const d = dist(frog.tongue.x, frog.tongue.y, fly.x, fly.y);
-    // Check if it's an overlap
-    const eaten = (d < frog.tongue.size/2 + fly.size/2);
+function checkTongueFliesOverlap() {
+   
+  for(let fly of flies){
+    const d= dist(frog.tongue.x, frog.tongue.y, fly.x, fly.y);
+  
+const eaten = (d < frog.tongue.size/2 + fly.size/2);
     if (eaten) {
         // Reset the fly
-        resetFly();
-        // Bring back the tongue
+        fly.x = 0;
+        fly.y = random(50,300);
         frog.tongue.state = "inbound";
     }
+}
 }
 
 /**
