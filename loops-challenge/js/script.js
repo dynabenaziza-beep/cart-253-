d6205bd0a1a69606e562fa4cdce23750ee2ba0f4
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-11-04 20:00:39 -0500

    chnaged variable so that java scrip can recognise it

--- loops-challenge/js/script.js
+++ loops-challenge/js/script.js
@@ -34,17 +34,17 @@ function draw() {
     //increase the value ( like in thr exemple )
     x += 50; // 50 pixel to the rigth 
     shade += 25; // shade for each line ( get ligther and ligther )
-
+    }
     // horizontal line 
 
     let y = 0 ; 
-    let shade= 0;
+    let tone = 0;
 
     while(y<= 500){
         stroke(shade);
         line(y,0,width,y);
         y+=50;
         tone+=25; 
-    }
+    
 }
 }
