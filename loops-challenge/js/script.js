9be444c6af7537f87e6c6b0f1c6d83fc502db446
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-11-04 19:35:26 -0500

    restarted the loop challenged
    added a starting position

---
+++ loops-challenge/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Lines
+ * Pippin Barr
+ * 
+ * A series of lines across the canvas
+ */
+
+"use strict";
+
+/**
+ * Creates the canvas
+ */
+function setup() {
+    createCanvas(500, 500);
+}
+
+/**
+ * Draws lines across the canvas with increasing thickness and
+ * gradually lightening colour
+ */
+function draw() {
+    background("pink");
+    
+    let  x = 0; //starting position 
+    let shade = 0; // start the stroke color 
+}
