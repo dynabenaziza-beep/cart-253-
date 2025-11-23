634907f38726b71e8863e7670db1a9c8c47d8345
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-11-23 17:48:15 -0500

    copy pasted the eye and pupils form mod jam to frog drawing

--- variation-jam/variation1.js
+++ variation-jam/variation1.js
@@ -24,12 +24,24 @@ functionDrawFrogvariation1();
 }
 
 function moveFrogVariation1() {
-    frog.body.x=mouseX
+    frog.body.x=mouseX;
 }
 
-functionDrawFrogvariation1(){
+function DrawFrogvariation1() {
     //Draw frog body 
     fill("00ff00");
     noStroke();
     ellipse(frog.body.x, frog.body.y,frog.body.size);
+
+    //draw the frog eye 
+  fill(255);
+  ellipse(frog.body.x - 60 , frog.body.y-90,80,80); //left eye 
+  ellipse(frog.body.x +60 ,frog.body.y -90 ,80,80 ); //rigth eye 
+
+  //draw frog puppil 
+  fill(0); //black
+  ellipse(frog.body.x - 60 , frog.body.y - 90 , 50, 50); //left puppil 
+ellipse(frog.body.x+60, frog.body.y -90, 50 ,50); //rigth puppil 
+
 }
+
