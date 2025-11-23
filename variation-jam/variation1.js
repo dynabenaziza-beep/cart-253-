4ef0d923c3dd0e2e61e35c4ca41fd50d3ddcab15
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-11-23 17:30:17 -0500

    reused same frog structure from my mod jam

---
+++ variation-jam/variation1.js
@@ -0,0 +1,18 @@
+/* variation 1 - Frod Detective */
+const frog = {
+    // The frog's body has a position and size
+    body: {
+        x: 320,
+        y: 520,
+        size: 220
+    },
+    // The frog's tongue has a position, size, speed, and state
+    tongue: {
+        x: undefined,
+        y: 480,
+        size: 20,
+        speed: 20,
+        // Determines how the tongue moves each frame
+        state: "idle" // State can be: idle, outbound, inbound
+    }
+};
