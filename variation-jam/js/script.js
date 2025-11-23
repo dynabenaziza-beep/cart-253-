af91e7ce949d8bdfd34cf63d14bbbde33ec10eff
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-11-23 14:38:41 -0500

    starting the menu page for the 3 button variation
    copy pasted the code (from pippin ) for the menu variation page( first page )

--- variation-jam/js/script.js
+++ variation-jam/js/script.js
@@ -1,24 +1,75 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+ * Variation jam */
 
 "use strict";
+/* say which screen we are on */
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+let state ="menu"
+
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Display the menu or the current variation
 */
 function draw() {
-
+    switch (state) {
+        case "menu":
+            menuDraw();
+            break;
+        case "red-variation":
+            redDraw();
+            break
+        case "green-variation":
+            greenDraw();
+            break;
+        case "blue-variation":
+            blueDraw();
+            break;
+    }
 }
+
+/**
+ * Listen for mouse pressed and call the function for it in the
+ * current state
+ */
+function mousePressed() {
+    switch (state) {
+        case "menu":
+            menuMousePressed();
+            break;
+        case "red-variation":
+            redMousePressed();
+            break
+        case "green-variation":
+            greenMousePressed();
+            break;
+        case "blue-variation":
+            blueMousePressed();
+            break;
+    }
+}
+
+/**
+ * Listen for keypressed and call the function for it in the
+ * current state
+ */
+function keyPressed(event) {
+    switch (state) {
+        case "menu":
+            menuKeyPressed(event);
+            break;
+        case "red-variation":
+            redKeyPressed(event);
+            break
+        case "green-variation":
+            greenKeyPressed(event);
+            break;
+        case "blue-variation":
+            blueKeyPressed(event);
+            break;
+    }
+}
+
