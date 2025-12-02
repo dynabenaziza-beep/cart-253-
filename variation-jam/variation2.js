5df6a98837383059843c27c8751c586134265147
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-12-02 17:50:44 -0500

    Implement WASD movement and boundaries for frog2

--- variation-jam/variation2.js
+++ variation-jam/variation2.js
@@ -13,6 +13,12 @@ function variation2Draw() {
     textSize(32);
     text("VARIATION 2 IS WORKING!", width/2, height/2);
 
+ // move frog with WASD
+    if (keyIsDown(87)) frog2.y -= frog2.speed; // W
+    if (keyIsDown(83)) frog2.y += frog2.speed; // S
+    if (keyIsDown(65)) frog2.x -= frog2.speed; // A
+    if (keyIsDown(68)) frog2.x += frog2.speed; // D
+
    
 }
 
