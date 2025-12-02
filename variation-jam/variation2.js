6b3dd0e3aae20dac39d7a6eec7534d1542bec0c0
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-12-02 17:22:30 -0500

    copy pasted the whole code form the begining
    for variation2

--- variation-jam/variation2.js
+++ variation-jam/variation2.js
@@ -1,1 +1,23 @@
+function variation2Draw() {
+    background("pink");
+
+    fill(0);
+    textSize(32);
+    text("VARIATION 2 IS WORKING!", width/2, height/2);
+
+    // draw frog
+    fill(0,255,0);
+    ellipse(width/2, height/2 - 100, 80);
+}
+
+function variation2MousePressed() {
+    if (state === "variation2") {
+        state = "menu";  // click anywhere to go back
+    }
+}
+
+function resetVariation2() {
+    // nothing needed, just switch state
+    state = "variation2";
+}
 
