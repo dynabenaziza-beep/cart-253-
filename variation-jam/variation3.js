42fd695bdb9d8528084a4d15e9dc2de14453520a
dynabenaziza-beep <dynabenaziza@gmail.com>
2025-12-03 07:47:31 -0500

    added garfield in draw+ background

--- variation-jam/variation3.js
+++ variation-jam/variation3.js
@@ -5,14 +5,15 @@ function resetVariation3() {
 }
 
 function variation3Draw() {
-    // later this will be the main draw for variation 3
-    background(200); // temp background so i see something
-    fill(0);
-    textAlign(CENTER, CENTER);
-    textSize(24);
-    text("variation 3 - garfield frog (work in progress)", width / 2, height / 2);
-}
+   
+    // warm background so I know I'm in this variation
+    background(255, 210, 160);
 
+    // draw the frog (still static)
+    drawGarfieldFrog();
+
+    
+}
 function variation3MousePressed() {
     // i'll use this later if i want to go back to menu or restart
 }
